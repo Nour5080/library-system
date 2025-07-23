84a42b6e53e0d2e9d90495003025eb5da3d2eb39
Nour Mohamed <161865218+Nour5080@users.noreply.github.com>
2025-07-23 04:36:35 +0300

    Update library_system.cpp

--- library_system.cpp
+++ library_system.cpp
@@ -1,460 +1,296 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
+#include <algorithm>
+#include <limits>
 using namespace std;
 
 const int MAX_books = 10;
 const int MAX_users = 10;
 
-bool is_text(string input) {
+bool is_text(const string& input) {
     if (input.empty()) return false;
-
-    for (int i = 0; i < input.length(); i++) {
-        char c = input[i];
-
-        if (!((c >= 'A' && c <= 'Z') ||
-            (c >= 'a' && c <= 'z') ||
-            c == ' ')) {
+    for (char c : input) {
+        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == ' '))
             return false;
-        }
     }
-
     return true;
 }
 
-
-
-bool is_number(string input) {
+bool is_number(const string& input) {
     if (input.empty()) return false;
-
-    for (int i = 0; i < input.length(); i++) {
-        char c = input[i];
-
-        if (c < '0' || c > '9') {
+    for (char c : input) {
+        if (c < '0' || c > '9')
             return false;
-        }
     }
-
     return true;
 }
 
+int read_number(const string& msg) {
+    string input;
+    while (true) {
+        cout << msg;
+        getline(cin, input);
+        if (is_number(input)) return stoi(input);
+        cout << "Invalid input. Please enter a number.\n";
+    }
+}
 
+string read_text(const string& msg) {
+    string input;
+    while (true) {
+        cout << msg;
+        getline(cin, input);
+        if (is_text(input)) return input;
+        cout << "Invalid input. Please use letters only.\n";
+    }
+}
 
-struct book {
+struct Book {
     string name;
-    int id;
-    int total_borrowed;
-    int total_quantity;
-    book() {
-        total_borrowed = total_quantity = 0;
-        id = -1;
-        name = " ";
-    }
+    int id, total_borrowed, total_quantity;
+
+    Book() : id(-1), total_borrowed(0), total_quantity(0), name(" ") {}
 
     void read() {
-        cout << "please enter book info: \n \n";
-        
-            while (true) {
-
-                string Id_input;
-                cout << "Enter Id:\n\n";
-                cin >> Id_input;
-                if (is_number(Id_input)) {
-                    id = stoi(Id_input);
-                    break;
-                }
-                else {
-                    cout << "invaild Id, please try again.\n\n";
-                }
-            }
-        
-
-        while (true) {
-
-            string Name_input;
-            cout << "Enter Name:\n\n";
-            cin >> Name_input;
-            if (is_text(Name_input)) {
-                name = Name_input;
-                break;
-            }
-            else {
-                cout << "invaild Name, please try again.\n\n";
-            }
-        }
-
-
-        while (true) {
-
-            string quantity_input;
-            cout << "Enter Quantity:\n\n";
-            cin >> quantity_input;
-            if (is_number(quantity_input)) {
-                total_quantity = stoi(quantity_input);
-                break;
-            }
-            else {
-                cout << "invaild spcialization, please try again.\n\n";
-            }
-        }
+        cout << "Please enter book info:\n";
+        id = read_number("Enter Id: ");
+        name = read_text("Enter Name: ");
+        total_quantity = read_number("Enter Quantity: ");
     }
 
-   
-    bool has_prefix(string prefix) {
-        if (name.size() < prefix.size()) {
-            return false;
-        }
-        for (int i = 0; i < prefix.size(); ++i) {
-            if (prefix[i] != name[i]) {
-                return false;
-            }
-        }
-        return true;
+    bool has_prefix(const string& prefix) {
+        return name.substr(0, prefix.size()) == prefix;
     }
 
     bool borrowed(int user_id) {
-        if (total_quantity == total_borrowed)
-            return false;
+        if (total_quantity == total_borrowed) return false;
         total_borrowed++;
         return true;
     }
 
-    void return_coby() {
-        if (total_borrowed > 0) {
-            total_borrowed--;
-        } else {
-            cout << "there is an error\n";
-        }
+    void return_copy() {
+        if (total_borrowed > 0) total_borrowed--;
+        else cout << "Error: no borrowed copies to return.\n";
     }
 
     void print() {
-        cout << " id = " << id << " name = " << name
-             << " total quantity = " << total_quantity
-             << " total borrowed = " << total_borrowed << endl;
+        cout << "ID: " << id << ", Name: " << name
+            << ", Total: " << total_quantity
+            << ", Borrowed: " << total_borrowed << endl;
     }
 };
 
-bool cmd_by_name(book &a, book &b) {
-    return a.name < b.name;
-}
+bool cmp_by_name(Book& a, Book& b) { return a.name < b.name; }
+bool cmp_by_id(Book& a, Book& b) { return a.id < b.id; }
 
-bool cmd_by_id(book &a, book &b) {
-    return a.id < b.id;
-}
-
-struct user {
+struct User {
     string name;
-    int borrowed_books_id[MAX_books]{};
     int id;
+    int borrowed_books_id[MAX_books];
     int len;
-    user() {
-        name = " ";
-        id = -1;
-        len = 0;
-    }
+
+    User() : id(-1), len(0), name(" ") {}
 
     void read() {
-        while (true) {
-
-            string Id_input;
-            cout << "Enter Id:\n\n";
-            cin >> Id_input;
-            if (is_number(Id_input)) {
-                id = stoi(Id_input);
-                break;
-            }
-            else {
-                cout << "invaild Id, please try again.\n\n";
-            }
-        }
-
-
-        while (true) {
-
-            string Name_input;
-            cout << "Enter Name:\n\n";
-            cin >> Name_input;
-            if (is_text(Name_input)) {
-                name = Name_input;
-                break;
-            }
-            else {
-                cout << "invaild Name, please try again.\n\n";
-            }
-        }
-
-
+        id = read_number("Enter user ID: ");
+        name = read_text("Enter user name: ");
     }
-
 
     void borrowed(int book_id) {
         borrowed_books_id[len++] = book_id;
     }
-    void return_coby(int book_id) {
-        bool clear = false;
+
+    void return_copy(int book_id) {
+        bool found = false;
         for (int i = 0; i < len; ++i) {
             if (borrowed_books_id[i] == book_id) {
                 for (int j = i + 1; j < len; ++j)
                     borrowed_books_id[j - 1] = borrowed_books_id[j];
-                clear = true;
-                --len;
+                len--;
+                found = true;
                 break;
             }
         }
-        if (!clear) {
-            cout << "ooops user " << name << " never borrowed book id " << book_id << endl;
+        if (!found) {
+            cout << "User " << name << " never borrowed book ID " << book_id << endl;
         }
     }
+
     bool is_borrowed(int book_id) {
-        for (int i = 0; i < len; ++i) {
-            if (book_id == borrowed_books_id[i]) {
+        for (int i = 0; i < len; ++i)
+            if (borrowed_books_id[i] == book_id)
                 return true;
-            }
-        }
         return false;
     }
+
     void print() {
         sort(borrowed_books_id, borrowed_books_id + len);
-        cout << "the user " << name << " his id " << id << " borrowed books ids: ";
-        for (int i = 0; i < len; ++i) {
+        cout << "User: " << name << " (ID: " << id << ") borrowed books: ";
+        for (int i = 0; i < len; ++i)
             cout << borrowed_books_id[i] << " ";
-        }
-        cout << "\n";
+        cout << endl;
     }
 };
 
-struct library_system {
-    int total_books;
-    book books[MAX_books]{};
-    int total_users;
-    user users[MAX_users]{};
-    library_system(){
-        total_books=0;
-        total_users=0;
-
-    }
+struct LibrarySystem {
+    int total_books = 0;
+    Book books[MAX_books];
+    int total_users = 0;
+    User users[MAX_users];
 
     void run() {
         while (true) {
-
             int choice = menu();
-            if (choice == 1) {
-                add_book();
-            } else if (choice == 2) {
-                search_book_by_prefix();
-            } else if (choice == 3) {
-                print_who_borrowed_book_by_name();
-            } else if (choice == 4) {
-                print_library_by_ID();
-            } else if (choice == 5) {
-                print_library_by_NAME();
-            } else if (choice == 6) {
-                add_user();
-            } else if (choice == 7) {
-                user_borrow_book();
-            } else if (choice == 8) {
-                user_return_book();
-            } else if (choice == 9) {
-                print_user();
-            }
-
+            if (choice == 1) add_book();
+            else if (choice == 2) search_books_by_prefix();
+            else if (choice == 3) print_who_borrowed_book_by_name();
+            else if (choice == 4) print_library_by_id();
+            else if (choice == 5) print_library_by_name();
+            else if (choice == 6) add_user();
+            else if (choice == 7) user_borrow_book();
+            else if (choice == 8) user_return_book();
+            else if (choice == 9) print_users();
+            else if (choice == 10) break;
         }
     }
 
-    bool vaild(int choice) {
-        return (1 <= choice && choice <= 10);
-    }
-     int menu() {
-        int choice ;
+    int menu() {
+        string input;
         while (true) {
-            cout << "\nLibrary Menu;\n";
-            cout << "1) add_book\n";
-            cout << "2) search_books_by_prefix\n";
-            cout << "3) print_who_borrowed_book_by_name\n";
-            cout << "4) print_library_by_id\n";
-            cout << "5) print_library_by_name\n";
-            cout << "6) add_user\n";
-            cout << "7) user_borrow_book\n";
-            cout << "8) user_return_book\n";
-            cout << "9) print_users\n";
-            cout << "10) Exit\n";
-            cout << "\nEnter your menu choice [1 - 10]: ";
-            
-            cin >> choice;
-
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(),'\n');
-                cout << "Invalid input. Please enter a number between 1 and 10.\n\n";
-                continue;
+            cout << "\nLibrary Menu:\n"
+                << "1) Add book\n"
+                << "2) Search books by prefix\n"
+                << "3) Print who borrowed book by name\n"
+                << "4) Print library by ID\n"
+                << "5) Print library by name\n"
+                << "6) Add user\n"
+                << "7) User borrow book\n"
+                << "8) User return book\n"
+                << "9) Print users\n"
+                << "10) Exit\n"
+                << "\nEnter your menu choice [1 - 10]: ";
+            getline(cin, input);
+            if (is_number(input)) {
+                int choice = stoi(input);
+                if (choice >= 1 && choice <= 10) return choice;
             }
-
-            if (!vaild(choice)) {
-                cout << "Invalid choice. Try again\n";
-                continue;                                                                // loop keep working
-            }
-            break;
+            cout << "Invalid input. Try again.\n";
         }
-        return choice;
     }
 
     void add_book() {
         books[total_books++].read();
     }
 
-    void search_book_by_prefix() {
-        cout << "enter book name prefix: ";
-        int count = 0;
-        string prefix;
-
-        while (true) {
-            string prefix_input;
-            cout << "Enter prefix:\n\n";
-            cin >> prefix_input;
-            if (is_text(prefix_input)) {
-                prefix = prefix_input;
-                break;
-            }
-            else {
-                cout << "invaild prefix, please try again.\n\n";
-            }
-        }
-
-        for (int i = 0; i < total_books; ++i) {
-            if (books[i].has_prefix(prefix)) {
-                cout << books[i].name;
-                count++;
-            }
-        }
-        if (count == 0) {
-            cout << "not found\n";
-        }
-    }
-
-    void print_library_by_ID() {
-        sort(books, books + total_books, cmd_by_id);
-        cout << "\n";
-        for (int i = 0; i < total_books; ++i)
-            books[i].print();
-
-    }
-
-    void print_library_by_NAME() {
-        sort(books, books + total_books, cmd_by_name);
-        cout << "\n";
-        for (int i = 0; i < total_books; ++i) {
-            books[i].print();
-        }
-    }
-
     void add_user() {
         users[total_users++].read();
     }
 
-    void print_user() {
-        cout << "\n";
-        for (int i = 0; i < total_users; ++i) {
-            users[i].print();
-        }
-    }
-
     int find_book(const string& name) {
-        for (int i = 0; i < total_books; ++i) {
-            if (name == books[i].name) {
+        for (int i = 0; i < total_books; ++i)
+            if (books[i].name == name)
                 return i;
-            }
-        }
-        return -1;
-    }
-    int find_user(const string& name){
-        for (int i = 0; i < total_users; ++i) {
-            if (name==users[i].name){
-                return i;
-            }
-
-        }
         return -1;
     }
 
+    int find_user(const string& name) {
+        for (int i = 0; i < total_users; ++i)
+            if (users[i].name == name)
+                return i;
+        return -1;
+    }
 
-    bool read_books_name_and_user_name(int &book_idx,int &user_idx,int trail=3){
-        string book_name;
-        string user_name;
-        while (trail--){
-            cout << "enter user name:\n \n";
-            cin >> user_name;
-            cout<< "enter book name: \n\n";
-            cin>> book_name;
-            user_idx= find_user(user_name);
+    bool read_book_and_user(int& book_idx, int& user_idx) {
+        for (int i = 0; i < 3; ++i) {
+            string user_name = read_text("Enter user name: ");
+            string book_name = read_text("Enter book name: ");
 
-            if(user_idx==-1){
-                cout<<"invalid user name! please try again\n";
+            user_idx = find_user(user_name);
+            if (user_idx == -1) {
+                cout << "Invalid user name.\n";
                 continue;
             }
-            book_idx= find_book(book_name);
-            if(book_idx==-1){
-                cout<<"invalid book name! please try again\n";
+
+            book_idx = find_book(book_name);
+            if (book_idx == -1) {
+                cout << "Invalid book name.\n";
                 continue;
             }
+
             return true;
         }
-        cout<<"you did several trials!! try later XP\n";
+        cout << "Too many failed attempts. Try again later.\n";
         return false;
     }
 
-    void print_who_borrowed_book_by_name(){
+    void user_borrow_book() {
+        int book_idx, user_idx;
+        if (!read_book_and_user(book_idx, user_idx)) return;
+        if (!books[book_idx].borrowed(users[user_idx].id))
+            cout << "No more copies available.\n";
+        else
+            users[user_idx].borrowed(books[book_idx].id);
+    }
 
-        string book_name;
-        cout<<"enter book name:\n";
-        cin>>book_name;
+    void user_return_book() {
+        int book_idx, user_idx;
+        if (!read_book_and_user(book_idx, user_idx)) return;
+        books[book_idx].return_copy();
+        users[user_idx].return_copy(books[book_idx].id);
+    }
 
-        int book_idx= find_book(book_name);
-        if(book_idx==-1){
-            cout<<"invalid book name\n";
-            return;
-        }
-        int book_id = books[book_idx].id;
-        if(books[book_idx].total_borrowed==0){
-            cout<<"no borrowed copies\n";
-            return;
-        }
-        for (int i = 0; i < total_users; ++i) {
-            if(users[i].is_borrowed(book_id)){
-                cout<<users[i].name;
+    void print_users() {
+        for (int i = 0; i < total_users; ++i)
+            users[i].print();
+    }
+
+    void print_library_by_id() {
+        sort(books, books + total_books, cmp_by_id);
+        for (int i = 0; i < total_books; ++i)
+            books[i].print();
+    }
+
+    void print_library_by_name() {
+        sort(books, books + total_books, cmp_by_name);
+        for (int i = 0; i < total_books; ++i)
+            books[i].print();
+    }
+
+    void search_books_by_prefix() {
+        string prefix = read_text("Enter book name prefix: ");
+        int count = 0;
+        for (int i = 0; i < total_books; ++i) {
+            if (books[i].has_prefix(prefix)) {
+                books[i].print();
+                count++;
             }
         }
-
+        if (count == 0)
+            cout << "No books found with that prefix.\n";
     }
-    void user_return_book() {
-        int book_idx,user_idx;
-        if(!read_books_name_and_user_name(book_idx,user_idx)){
+
+    void print_who_borrowed_book_by_name() {
+        string book_name = read_text("Enter book name: ");
+        int book_idx = find_book(book_name);
+        if (book_idx == -1) {
+            cout << "Invalid book name.\n";
             return;
         }
-        int book_id=books[book_idx].id;
-        books[book_idx].return_coby();
-        users[user_idx].return_coby(book_id);
 
-    }
-    void user_borrow_book() {
-        int user_idx,book_idx;
-        if(!read_books_name_and_user_name(book_idx,user_idx)){
+        int book_id = books[book_idx].id;
+        if (books[book_idx].total_borrowed == 0) {
+            cout << "No copies are currently borrowed.\n";
             return;
         }
-        int user_id=users[user_idx].id;
-        int book_id=books[book_idx].id;
-        if(!books[book_idx].borrowed(user_id)){
-            cout<<"no more copies available right now\n";
-        }
-        else{
-            users[user_idx].borrowed(book_id);
-        }
 
+        for (int i = 0; i < total_users; ++i)
+            if (users[i].is_borrowed(book_id))
+                cout << users[i].name << endl;
     }
-
 };
 
 int main() {
-    library_system library ;
+    LibrarySystem library;
     library.run();
     return 0;
 }
